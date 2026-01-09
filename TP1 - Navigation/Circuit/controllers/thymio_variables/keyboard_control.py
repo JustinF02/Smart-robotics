096b31ba7c3f0669441fa520f6e9187edbb34817
Ferdinand Justin <73842494+JustinF02@users.noreply.github.com>
2026-01-09 15:03:58 +0100

    fixed keyboard control

--- TP1 - Navigation/Circuit/controllers/thymio_variables/keyboard_control.py
+++ TP1 - Navigation/Circuit/controllers/thymio_variables/keyboard_control.py
@@ -27,7 +27,8 @@ robot = Robot()
 timestep = int(robot.getBasicTimeStep())
 keyboard = robot.getKeyboard()
 keyboard.enable(timestep)
-robot_speed = 1
+robot_speed = 0
+rotation_speed = 1
 
 print(chr(27) + "[2J") # ANSI code for clearing command line
 print("Initialization of thymio_variables controller")
@@ -57,37 +58,40 @@ def wait_ms(duration_ms):
 while (robot.step(timestep) != -1):
   
   #Set motors speed :
-  motor_left.setVelocity(1)
-  motor_right.setVelocity(1)
+  #motor_left.setVelocity(1)
+  #motor_right.setVelocity(1)
+  left_speed = robot_speed
+  right_speed = robot_speed
+    
 
   # Process sensor data here
   prox_values = [ s.getValue() for s in prox_sensors ]
 
   print("Proximité :", prox_values)
 
-  #Enter here functions to send actuator commands, like:
   command = keyboard.getKey()
   
   print(command)
 
-  if command==keyboard.LEFT:
-    print('Left')
-    motor_left.setVelocity(0.0) #-robot_speed
-    motor_right.setVelocity(robot_speed)
-  elif command==keyboard.RIGHT:
-    print('right')
-    motor_left.setVelocity(robot_speed)
-    motor_right.setVelocity(0.0) #-robot_speed
-  elif command==keyboard.UP:
-    print('up')
-    if robot_speed<2:
-      robot_speed+=0.2
-  elif command==keyboard.DOWN:
-    print('down')
-    if robot_speed>-2:
-      robot_speed-=0.2
-  elif command==83: # capture S key
-    print('stop')
+  if command == keyboard.UP:
+    robot_speed += 0.2
+  if robot_speed > 9.5: robot_speed = 9.5 # Limite Thymio
+  elif command == keyboard.DOWN:
+    robot_speed -= 0.2
+    if robot_speed < -9.5: robot_speed = -9.5
+  elif command == keyboard.LEFT:
+    left_speed = -rotation_speed
+    right_speed = rotation_speed
+  elif command == keyboard.RIGHT:
+    left_speed = rotation_speed
+    right_speed = -rotation_speed
+  elif command == 83: # Touche 'S'
     robot_speed = 0
-
+    left_speed = 0
+    right_speed = 0
+        
+  motor_left.setVelocity(left_speed)
+  motor_right.setVelocity(right_speed)
+  print(f"actual speed: {robot_speed:.2f}")
+#4 = 5.4
 # Enter here exit cleanup code
